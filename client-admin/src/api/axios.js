058c0d6c634cf0b022aa4de86bf75fa843c32159
davidmnhat <vominhnhat181005@gmail.com>
2026-05-08 09:57:03 +0700

    update

--- client-admin/src/api/axios.js
+++ client-admin/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL // Dòng này là quan trọng nhất!
+  baseURL: '/api'
 });
 
 // Tự động thêm Token vào mỗi request
